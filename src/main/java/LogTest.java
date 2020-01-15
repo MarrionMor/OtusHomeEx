2c3dd5be62a4cad5726c45312e5e371036f3704f
marrionmor <masha_914@mail.ru>
2020-01-15 22:32:23 +0300

    logger

---
+++ src/main/java/LogTest.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogTest {
+    private static final Logger logger = LogManager.getLogger(LogTest.class);
+
+    public static void main (String[] arg){
+        logger.info("JAVA_HOME: {}" , System.getProperty("java_home"));
+        logger.info("OS: {}", System.getProperty("os_name"));
+    }
+}
